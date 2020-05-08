85703cd646cbf2912ab9f69873adc26e7d8fc321
gjacopo <gjacopo@gmail.com>
2020-05-08 15:23:03 +0200

    initialisation file

---
+++ pydumbtools/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+.. __init__
+
+Initiatlisation module for package `pydumdutils` introducing various basic utilities.
+"""
+
+# *credits*:      `gjacopo <jacopo.grazzini@ec.europa.eu>`_ 
+# *since*:        Fri May  8 14:30:29 2020
+
+
+
+__all__ = ['log', 'misc', 'parse', 'type', 'object', 'file']
+
